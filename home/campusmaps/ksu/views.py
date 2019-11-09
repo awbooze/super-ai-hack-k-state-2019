58960074e466b6cf2500764a85b4c4c444cb5f96
Andrew Booze <42682253+awbooze@users.noreply.github.com>
2019-11-09 15:55:19 -0600

    Add base HTML template and CSS

--- home/campusmaps/ksu/views.py
+++ home/campusmaps/ksu/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from .models import *
-# Create your views here.
 
+# Create your views here.
 def index(request):
     building_list = Building.objects.order_by('-name')
     context = {'building_list':building_list}
     return render(request, 'ksu/index.html', context)
     
 def floors(request, building_name):
-    return HttpResponse("You're looking at the floors for %s Hall" % building_name)
+    floor_list = Floor.objects.order_by('number')
+    context = {'floor_list': floor_list, 'building_name': building_name}
+    return render(request, 'ksu/floors.html', context)
 
 def map(request, building_name, floor_id):
     return HttpResponse("You're looking at the rooms for %s Floor %i" % (building_name, floor_id))
