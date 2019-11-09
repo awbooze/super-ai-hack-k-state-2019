24a91084eb542f649d511918e0cf37e3c3ee0487
Andrew Booze <42682253+awbooze@users.noreply.github.com>
2019-11-09 12:02:46 -0600

    Add initial models for Building, Floor, and Room

--- home/campusmaps/campusmaps/settings.py
+++ home/campusmaps/campusmaps/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2bi%x-k$$rv-^^n@c&pn__du4%4m08y7hy&)2$o_ql0*9c#8#f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ksu.apps.KsuConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- home/campusmaps/ksu/models.py
+++ home/campusmaps/ksu/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Building(models.Model):
+    name = models.CharField(max_length=200)
+    abbreviation = models.CharField(max_length=10)
+    # store reference to svg file here?
+    
+    def __str__(self):
+        return self.name
+
+class Floor(models.Model):
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    number = models.IntegerField(default=-1)
+    # store reference to svg file here?
+    
+    def __str__(self):
+        return self.number
+
+class Room(models.Model):
+    floor = models.ForeignKey(Floor, on_delete=models.CASCADE)
+    number = models.CharField(max_length=10)
+    x = models.IntegerField(default=0)
+    y = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.number
