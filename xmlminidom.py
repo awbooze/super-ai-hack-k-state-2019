e3fc9da2bb6aa39ef9ac62070c0c7809f12c7f7a
Marissa Shivers <mxs@ksu.edu>
2019-11-09 15:26:33 -0600

    fix formatting

--- xmlminidom.py
+++ xmlminidom.py
@@ -21,6 +21,10 @@ from xml.dom import minidom
 
 # doc.unlink()
 
+'''
+Cleans an .SVG file by removing all text and then
+attempting to remove as much as the surrounding bubble as possible
+'''
 def clean_svg(svg):
     # Removes all text
     doc = minidom.parse(svg)
@@ -30,43 +34,14 @@ def clean_svg(svg):
         parent = node.parentNode
         parent.removeChild(node)
 
-    '''
-    Remove all elements containing 
-    fill:none;stroke:#000000;stroke-width:6;stroke-linecap:
-    round;stroke-linejoin:round;stroke-miterlimit:10;
-    stroke-dasharray:none;stroke-opacity:1
 
-    inside element g -> inside element path -> attribute = style
-    '''
+    # Remove bubbles where the numbers go inside of
+    # mostly works...
+    # inside element g -> inside element path -> attribute = style
     gElements = doc.getElementsByTagName('g')
     for g in gElements:
         pathElements = g.getElementsByTagName('path')
         for path in pathElements:
-            # curStyleString = path.getAttribute('style')
-            # if ((curStyleString == 'fill:none;stroke:#000000;stroke-width:6;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;stroke-dasharray:none;stroke-opacity:1')
-            #                 and (((path.getAttribute('d') == 'M 0,84.5 C -46.66806,84.5 -84.5,46.66806 -84.5,0 -84.5,-46.66806 -46.66806,-84.5 0,-84.5'))
-            #                 or (path.getAttribute('d') == 'M 0,-84.5 C 46.66806,-84.5 84.5,-46.66806 84.5,0 84.5,46.66806 46.66806,84.5 0,84.5'))
-            #                 or ('v -270' in path.getAttribute('d'))
-            #                 or ('v 270' in path.getAttribute('d'))
-            #                 or ('v -523' in path.getAttribute('d'))
-            #                 or ('v 523' in path.getAttribute('d'))
-            #                 or ('v -524' in path.getAttribute('d'))
-            #                 or ('v 524' in path.getAttribute('d'))):            
-            #     parent = path.parentNode # g
-            #     parent.removeChild(path) # remove child of g
-            # if ((path.getAttribute('style') == 'fill:none;stroke:#000000;stroke-width:6;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;stroke-dasharray:none;stroke-opacity:1')
-            #                 and (((path.getAttribute('d') == 'M 0,-83.5 C 46.11578,-83.5 83.5,-46.11578 83.5,0 83.5,46.11578 46.11578,83.5 0,83.5'))
-            #                 or (path.getAttribute('d') == 'M 0,83.5 C -46.11578,83.5 -83.5,46.11578 -83.5,0 -83.5,-46.11578 -46.11578,-83.5 0,-83.5'))
-            #                 or ('v -268' in path.getAttribute('d'))
-            #                 or ('v 268' in path.getAttribute('d'))
-            #                 or ('v -267' in path.getAttribute('d'))
-            #                 or ('v 267' in path.getAttribute('d'))
-            #                 or ('v 519' in path.getAttribute('d'))
-            #                 or ('v -519' in path.getAttribute('d'))
-            #                 or ('v 518' in path.getAttribute('d'))
-            #                 or ('v -518' in path.getAttribute('d'))):
-            #     parent = path.parentNode # g
-            #     parent.removeChild(path) # remove child of g
 
             # test path.getAttribute('d') if it starts with 'm'
             if (path.getAttribute('style') == 'fill:none;stroke:#000000;stroke-width:6;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;stroke-dasharray:none;stroke-opacity:1'):
@@ -83,11 +58,8 @@ def clean_svg(svg):
                     if (pieces[0] == 'M' and pieces[2] == 'C'):
                         parent = path.parentNode # g
                         parent.removeChild(path) # remove child of g
-
-            # M 0,-83.5 C 46.11578,-83.5 83.5,-46.11578 83.5,0 83.5,46.11578 46.11578,83.5 0,83.5
             
-
-    # Writes to a new SVG pile
+    # Writes to a new SVG file
     doc.writexml(open('cleaned.svg', 'w'),
                indent="  ",
                addindent="  ",
