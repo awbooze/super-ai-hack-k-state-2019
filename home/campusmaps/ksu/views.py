6abddd59a0ed67d9f8eb1ff53ec2989b1fd8adff
Andrew Booze <42682253+awbooze@users.noreply.github.com>
2019-11-09 19:19:21 -0600

    Update views.py with more comments

--- home/campusmaps/ksu/views.py
+++ home/campusmaps/ksu/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import *
 
-# Create your views here.
+# Index page
 def index(request):
     building_list = Building.objects.order_by('name')
     context = {'building_list':building_list}
     return render(request, 'ksu/index.html', context)
     
+# List of floors
 def floors(request, building_name):
     b = get_object_or_404(Building, name=building_name)
     floor_list = Floor.objects.filter(building = b).order_by('number')
@@ -16,11 +17,14 @@ def floors(request, building_name):
 
     return render(request, 'ksu/floors.html', context)
 
+# Map of floor page
 def map(request, building_name, floor_id):
     building = get_object_or_404(Building, name=building_name)
     floor = get_object_or_404(Floor, number=floor_id)
     context = {"floor":floor}
     return render(request, 'ksu/map.html', context)
+
+# Search page (same as map page)
 def search(request, building_name, room_number):
     room=get_object_or_404(Room, number=room_number)
     floor = room.floor
