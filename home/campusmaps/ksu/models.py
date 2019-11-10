a81e400f8e91459b03edc5e57353c5b6cc263f81
Ben Thompson <bathompson@ksu.edu>
2019-11-09 18:37:50 -0600

    fixed small issue

--- home/campusmaps/ksu/models.py
+++ home/campusmaps/ksu/models.py
@@ -11,7 +11,7 @@ class Building(models.Model):
 class Floor(models.Model):
     building = models.ForeignKey(Building, on_delete=models.CASCADE)
     number = models.IntegerField(default=-1)
-    # file = models.CharField(max_length=200)
+    # filePath = models.CharField(max_length=200)
     
     def __str__(self):
         return str(self.number)
